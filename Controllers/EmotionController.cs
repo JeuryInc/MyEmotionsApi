8ee5b9cd20b100a64c64c71b98d6446ab5c25cc6
Geurys Antonio De la Cruz Lázaro <gdelacruz@sb.gob.do>
2022-01-06 12:24:49 -0400

    adding some validation to controller

--- Controllers/EmotionController.cs
+++ Controllers/EmotionController.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using MyEmotionsApi.API.ViewModels;
 using MyEmotionsApi.Data.Interfaces;
 using MyEmotionsApi.Entities;
@@ -16,69 +17,139 @@ namespace MyEmotionsApi.Controllers
     public class EmotionController : ControllerBase
     {
         readonly IEmotionRepository _emotionRepository;
+        private readonly ILogger _logger;
         readonly IMapper _mapper;
 
         public EmotionController(
             IEmotionRepository emotionRepository,
+            ILogger logger,
             IMapper mapper
         )
         {
             _emotionRepository = emotionRepository;
+            _logger = logger;
             _mapper = mapper;
         }
 
         [HttpGet("GetEmotions")]
         public ActionResult<List<EmotionViewModel>> GetEmotions()
         {
-            var emotions = _emotionRepository.AllIncluding(s => s.Owner).OrderByDescending(x => x.CreationTime);
+            try
+            {
+                var emotions = _emotionRepository.AllIncluding(s => s.Owner).Where(x => x.IsPublic).OrderByDescending(x => x.CreationTime);
 
-            return emotions.Select(_mapper.Map<EmotionViewModel>).ToList();
+                return emotions.Select(_mapper.Map<EmotionViewModel>).ToList();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Exception thrown while getting the emotions: {ex}");
+            }
+
+            return new BadRequestResult();
         }
 
         [HttpGet("GetEmotionsByTag/{tag}")]
         public ActionResult<List<EmotionViewModel>> GetEmotionsByTag(string tag)
         {
-            var emotions = _emotionRepository.GetAll().Where(x => x.Tags.FirstOrDefault().Contains(tag))?.ToList();
+            try
+            {
+                if (string.IsNullOrWhiteSpace(tag))
+                    return new List<EmotionViewModel>();
 
-            return emotions.Select(_mapper.Map<EmotionViewModel>).ToList();
+                var emotions = _emotionRepository.AllIncluding(s => s.Owner).Where(x => x.Tags.FirstOrDefault().Contains(tag));
+
+                return emotions.Select(_mapper.Map<EmotionViewModel>).ToList();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Exception thrown while getting the emotions tags: {ex}");
+            }
+
+            return new BadRequestResult();
         }
 
         [HttpGet("GetTags")]
         public ActionResult<List<string>> GetTags()
         {
-            var tags = new List<string>();
+            try
+            {
+                var emotions = _emotionRepository.GetAll().Where(x => !string.IsNullOrEmpty(x.Content) && x.IsPublic).ToList();
 
-            var emotions = _emotionRepository.GetAll().Where(x=> !string.IsNullOrEmpty(x.Content)).ToList();              
+                return GetTop20Tag(emotions).ToList();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Exception thrown while getting the top 20 tags: {ex}");
+            }
+
+            return new BadRequestResult();
+        }
+
+        private IEnumerable<string> GetTop20Tag(List<Emotion> emotions)
+        {
+            var tags = new List<string>();
 
             emotions.ForEach((e) =>
             {
-                tags.AddRange(e.Tags.FirstOrDefault().Split(' '));
+                var tag = e.Tags.FirstOrDefault();
+
+                if (tag != null)
+                    tags.AddRange(tag.Split(','));
             });
 
-            var groups = tags.GroupBy(v => v).OrderByDescending(x=> x.Count()).Take(20).Select(x=> x.Key);
-             
-            return groups.ToList();
+            var groups = tags.GroupBy(v => v).OrderByDescending(x => x.Count()).Take(20).Select(x => x.Key);
+
+            return groups;
         }
 
         [HttpGet("GetEmotionDetail/{id}")]
         public ActionResult<EmotionViewModel> GetEmotionDetail(string id)
         {
-            var story = _emotionRepository.GetSingle(s => s.Id == id, s => s.Owner);
-            var userId = HttpContext.User.Identity.Name;
+            try
+            {
+                if (string.IsNullOrWhiteSpace(id))
+                    return new BadRequestResult();
 
-            return _mapper.Map<Emotion, EmotionViewModel>(
-                story
-            );
+                var emotion = _emotionRepository.GetSingle(s => s.Id == id, s => s.Owner);
+
+                var userId = HttpContext.User.Identity.Name;
+
+                return _mapper.Map<Emotion, EmotionViewModel>(
+                    emotion
+                );
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Exception thrown while getting the top 20 tags: {ex}");
+            }
+
+            return new BadRequestResult();
+
         }
 
         [HttpGet("GetEmotionsByUser")]
         public ActionResult<List<EmotionViewModel>> GetEmotionsByUser()
         {
-            var user = HttpContext.User.Identity.Name;
+            string user = string.Empty;
 
-            var emotions = _emotionRepository.GetAll().Where(x => x.OwnerId == user).OrderByDescending(x=> x.CreationTime).ToList();
+            try
+            {
+                user = HttpContext.User.Identity.Name;
 
-            return emotions.Select(_mapper.Map<EmotionViewModel>).ToList();
+                if (string.IsNullOrEmpty(user))  
+                    return new BadRequestResult();                 
+
+                var emotions = _emotionRepository.GetAll().Where(x => x.OwnerId == user).OrderByDescending(x => x.CreationTime).ToList();
+
+                return emotions.Select(_mapper.Map<EmotionViewModel>).ToList();
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Exception thrown while getting the emotions saved by user {user}: {ex}");
+            }
+
+            return new BadRequestResult();
         }
 
         [HttpPost("Create")]
@@ -88,10 +159,11 @@ namespace MyEmotionsApi.Controllers
 
             var ownerId = HttpContext.User.Identity.Name;
             var creationTime = DateTime.UtcNow;
-            var storyId = Guid.NewGuid().ToString();
-            var story = new Emotion
+            var emotionId = Guid.NewGuid().ToString();
+            
+            var emotion = new Emotion
             {
-                Id = storyId,
+                Id = emotionId,
                 Title = model.Title,
                 Content = model.Content,
                 Tags = model.Tags,
@@ -99,23 +171,11 @@ namespace MyEmotionsApi.Controllers
                 OwnerId = ownerId
             };
 
-            _emotionRepository.Add(story);
+            _emotionRepository.Add(emotion);
+
             _emotionRepository.Commit();
 
-            return Ok(new { id = storyId});
-        }
-
-        [HttpDelete("{id}")]
-        public ActionResult Delete(string id)
-        {
-            var ownerId = HttpContext.User.Identity.Name;
-            if (!_emotionRepository.IsOwner(id, ownerId)) return Forbid("You are not the owner of this story");
-
-            _emotionRepository.DeleteWhere(story => story.Id == id);
-            _emotionRepository.Commit();
-
-            return NoContent();
-
+            return Ok(new { id = emotionId });
         }
     }
 }
